d94754299753cd26b80a99cacb9cb541ce4a1e8f
joaolavoier-luizalabs <joao.filho@luizalabs.com>
2018-10-03 22:13:58 -0300

    Implement heroku deploy

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var webpackRules = require('./scripts/webpack.rules');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
-  root: path.resolve(__dirname, 'build'),
+  root: path.resolve(__dirname),
   build: path.resolve(__dirname, 'build'),
-  src: path.resolve(__dirname, 'src')
+  src: path.resolve(__dirname, 'src'),
+  templates: path.resolve(__dirname, 'templates')
 }
 
 module.exports = {
@@ -15,12 +17,18 @@ module.exports = {
       path.resolve('./node_modules')
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PATHS.templates + '/index.html',
+      title: '[DEV] - Olist',
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: PATHS.build
   },
   devServer: {
-    contentBase: PATHS.root,
+    contentBase: PATHS.build,
     compress: true,
     port: 8888
   },
